2f8bc1cc7eecf446351c124254210d9f97e364aa
morenamartin <martinmorena269@gmail.com>
2025-09-24 14:22:04 -0300

    agrego speedinsights de vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import WhatsApp from "@/components/whatsapp";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -30,6 +31,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <SpeedInsights />
         <WhatsApp />
       </body>
     </html>
