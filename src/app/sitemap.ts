9113573a6708d120d9dffd864bdff9210687706d
morena-martin <morena@shaper.plus>
2025-10-08 22:28:49 -0300

    update sitemap URLs to point to the new domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,25 +4,25 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://ascensores-mg.vercel.app',
+      url: 'https://www.ascensoresmgsrl.com.ar/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://ascensores-mg.vercel.app/contacto',
+      url: 'https://www.ascensoresmgsrl.com.ar/contacto',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://ascensores-mg.vercel.app/quienes-somos',
+      url: 'https://www.ascensoresmgsrl.com.ar/quienes-somos',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
     },
     {
-      url: 'https://ascensores-mg.vercel.app/preguntas',
+      url: 'https://www.ascensoresmgsrl.com.ar/preguntas',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.7,
